2765a5b5229ed4703ab64bdfd44d28374e037f97
Sune Kirkeby <sune@moria.(none)>
2009-03-15 14:09:02 +0100

    Control the moc server.

--- moc.py
+++ moc.py
@@ -10,9 +10,12 @@ import socket
 import asyncore
 import struct
 import traceback
+import logging
 
 import _moc_protocol
 from _moc_protocol import *
+
+log = logging.getLogger(__name__)
 
 event_names = dict((getattr(_moc_protocol, name), name)
                    for name in dir(_moc_protocol)
@@ -31,7 +34,7 @@ state_names = dict((getattr(_moc_protocol, name), name)
 int_format = 'i'
 int_length = struct.calcsize(int_format)
 long_format = 'l'
-long_length = struct.calcsize(int_format)
+long_length = struct.calcsize(long_format)
 max_recv = 1024 * 1024
 
 tags_keys = 'title', 'artist', 'album', 'track', 'time', 'filled'
@@ -45,6 +48,7 @@ message_formats = {
     EV_PLIST_MOVE: ('string', 'string'),
     EV_FILE_TAGS: ('string',) + tags_message_format,
 }
+empty_values = {'int': 0, 'time': 0, 'string': ''}
 
 # FIXME - The taking and having methods should be merged, so we have
 # take_complete_message which either takes a complete message, or leaves the
@@ -62,7 +66,8 @@ class AsyncoreMocProtocol(asyncore.dispatcher):
 
     def handle_connect(self):
         self.client.on_connect()
-        self.unsent += struct.pack(int_format, CMD_SEND_EVENTS)
+        self.send_int(CMD_SEND_EVENTS)
+        self.get_state(self.client.on_state_changed)
 
     def writable(self):
         return bool(self.unsent)
@@ -85,10 +90,10 @@ class AsyncoreMocProtocol(asyncore.dispatcher):
         handler = getattr(self, 'handle_' + event.lower(),
                           lambda: event_id in data_less_events)
         if not handler():
-            print 'unhandled', event
+            log.warn('Unhandled event %s', event)
             if self.received:
-                print 'dazed and confused, trying to continue'
-                print 'lusering received data:', repr(self.received)
+                log.error('Lusering received data: %r', self.received)
+                log.error('Dazed and confused, trying to continue')
                 self.received = ''
 
     def have_complete_message(self, event_id, offset):
@@ -142,9 +147,9 @@ class AsyncoreMocProtocol(asyncore.dispatcher):
     def take_time(self):
         return struct.unpack(long_format, self.take(long_length))[0]
     def take_tags(self):
-        return dict(zip(tags_keys, self.take_message(tags_format)))
+        return dict(zip(tags_keys, self.take_message(tags_message_format)))
     def take_item(self):
-        return dict(zip(item_keys, self.take_message(item_format)))
+        return dict(zip(item_keys, self.take_message(item_message_format)))
     def take_message(self, message_format):
         return [ getattr(self, 'take_' + piece_format)()
                  for piece_format in message_format ]
@@ -175,47 +180,152 @@ class AsyncoreMocProtocol(asyncore.dispatcher):
         self.client.on_file_tags(self.take_string(), self.take_tags())
         return True
 
+    def send_int(self, i):
+        self.unsent += struct.pack(int_format, i)
+    def send_time(self, t):
+        self.unsent += struct.pack(long_format, t)
+    def send_string(self, s):
+        self.send_int(len(s))
+        self.unsent += s
+    def send_item(self, item):
+        for key, type in zip(item_keys, item_message_format):
+            getattr(self, 'send_' + type)(item.get(key, empty_values[type]))
     def send_command(self, command, callback, message_format):
         self.expected_ev_datas.append((message_format, callback))
-        self.unsent += struct.pack(int_format, command)
+        self.send_int(command)
 
+    def set_option(self, name, value):
+        self.send_int(CMD_SET_OPTION)
+        self.send_string(name)
+        self.send_int(value)
+    
+    def play(self, path):
+        self.send_int(CMD_STOP)
+        self.send_int(CMD_LIST_CLEAR)
+        self.send_int(CMD_LIST_ADD)
+        self.send_string(path)
+        self.send_int(CMD_PLAY)
+        self.send_string('')
+
+    def get_option(self, name, callback):
+        self.send_command(CMD_GET_OPTION, callback, ('int',))
+        self.send_string(name)
     def get_state(self, callback):
         self.send_command(CMD_GET_STATE, callback, ('state',))
     def get_song_name(self, callback):
         self.send_command(CMD_GET_SNAME, callback, ('string',))
 
     def handle_error(self):
-        traceback.print_exc()
+        log.error('Rome is burning!', exc_info=True)
+
+class MusicLibrary(object):
+    def __init__(self, songs):
+        self.songs = []
+        self._path_index = {}
+
+        for song in songs:
+            self.add(song)
+
+    def add(self, song):
+        self._path_index[song['path']] = len(self.songs)
+        self.songs.append(song)
+
+    # by-index accessors
+    def __len__(self):
+        return len(self.songs)
+    def get_at(self, index):
+        return self.songs[index]
+    def index(self, path):
+        return self._path_index[path]
+
+class Player(object):
+    def __init__(self, library):
+        self.library = library
+        self.playing = None
+
+    def on_playing(self, path):
+        try:
+            i = self.library.index(path)
+            self.playing = self.library.get_at(i)
+        except KeyError:
+            # FIXME - need a value to represent unknown?
+            log.info('playing unknown song: %r', path)
+            self.playing = None
+
+    def next(self):
+        if self.playing:
+            i = self.library.index(self.playing['path'])
+            return self.library.get_at((i + 1) % len(self.library))
+        else:
+            return self.library.get_at(0)
 
 class MocClient(object):
-    def __init__(self, protocol):
+    def __init__(self, protocol, player):
         self.protocol = protocol
+        self.state = 'STATE_NONE'
         self.status = None
+        self.player = player
 
     def on_connect(self):
-        self.protocol.get_song_name(self.on_playing)
+        log.debug('on_connect')
+        log.debug('disabling AutoNext')
+        self.protocol.set_option('AutoNext', 0)
 
     def on_state_changed(self, state):
-        if state == 'STATE_PLAY':
+        transition = (self.state + '->' + state).replace('STATE_', '').lower()
+        log.debug('on_state_changed: ' + transition)
+        self.state = state
+        if transition == 'stop->play' or transition == 'none->play':
             self.protocol.get_song_name(self.on_playing)
-        else:
-            print 'state', state
+        elif transition == 'none->stop':
+            # Connected to a stopped moc server, start it!
+            self.play_next_song()
+        elif transition == 'play->stop' and self.status <> 'Opening...':
+            # This is magically fucked up. The moc-server is multi-threaded,
+            # and wether we get play->play or play->stop->play state
+            # transitions, when some other player sends a CMD_PLAY, depends on
+            # timing, luck and the phase of the moon. But, 9 times out of 10 we
+            # get an 'Opening...' status-message from the server, before a
+            # play->stop->play transition. So, this will work as expected in
+            # most cases, but it is not fool-proof. Blame the morons who
+            # "designed" the brain damage that is the moc protocol.
+            self.play_next_song()
+
     def on_playing(self, path):
-        print 'playing', path
+        log.debug('on_playing: ' + repr(path))
+        if path:
+            self.player.on_playing(path)
     def on_status_msg(self, msg):
+        log.debug('on_status_msg: %r', msg)
         self.status = msg
-    def on_plist_add(self, item):
-        print 'plist_add', repr(item)
-    def on_plist_del(self, path):
-        print 'plist_del', path
-    def on_plist_move(self, old_path, new_path):
-        print 'plist_move', old_path, '->', new_path
+    def on_playlist_add(self, item):
+        log.debug('on_playlist_add: %r', item)
+    def on_playlist_del(self, path):
+        log.debug('on_playlist_del: %r', path)
+    def on_playlist_move(self, old_path, new_path):
+        log.debug('on_playlist_move: %r -> %r', old_path, new_path)
     def on_file_tags(self, path, tags):
-        print 'file_tags', path, ':', repr(self.tags)
+        log.debug('on_file_tags: %r: %r', path, tags)
+
+    def play_next_song(self):
+        log.debug('playing next song')
+        self.protocol.play(self.player.next()['path'])
 
 if __name__ == '__main__':
+    logging.basicConfig(level=0)
+    logging.getLogger().setLevel(0)
+
+    import os
+    import sys
+    song_dirs = sys.argv[1:]
+    songs = [ dict(path=os.path.join(song_dir, filename))
+              for song_dir in song_dirs
+              for filename in os.listdir(song_dir) ]
+    library = MusicLibrary(songs)
+    player = Player(library)
+
     protocol = AsyncoreMocProtocol()
-    client = MocClient(protocol)
+    client = MocClient(protocol, player)
     protocol.client = client
 
     protocol.create_socket(socket.AF_UNIX, socket.SOCK_STREAM)
